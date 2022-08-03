0544d37b3eef3efaf8669983b20b55a96e61f7bb
sireclifford <dev.sire.clifford@gmail.com>
2022-08-03 07:41:53 +0000

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
